dda2a6ccab6fac2e20f1c1bd4cd27897079458b6
sagynaliev <240118001@sdu.edu.kz>
2025-12-06 21:46:47 +0500

    Update server.js

--- TeachI/backend/server.js
+++ TeachI/backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 async function startServer() {
   try {
     // ✅ НАҚТЫ ПАРОЛЬ!
-    const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://240118001_db_user:Alikhan2007@teachi.6bvwzai.mongodb.net/?retryWrites=true&w=majority&appName=TeachI';
+    const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://240118001_db_user:TeachI2024!@teachi.6bvwzai.mongodb.net/?retryWrites=true&w=majority&appName=TeachI';
     
     console.log('🔧 Connecting to MongoDB...');
     
